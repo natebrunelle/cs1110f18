72e3712bc53bec68656ed1e734405885ab9b4108
Raymond Pettit <43241221+raypettit57@users.noreply.github.com>
2019-09-13 16:23:56 -0400

    Create Day08.py

---
+++ markdown/files/cs1110-001f19/Day08.py
@@ -0,0 +1,64 @@
+
+user1 = input("Please enter a number: ")  # input always returns a value of type string
+print("You typed in", user1)
+print(user1+user1)
+
+int(user1)
+
+# str1 = "bubble"
+# str2 = "will"
+#
+# print(str1, str2)
+#
+# str3 = str1 + " " + str2  # concatenation
+# print(str3)  # the plus sign is an "overloaded" operator
+#
+# x = 2
+# y = 5
+# print(str1*8)
+
+
+
+# x = 1  # this is an assignment statement
+# x = 2  # read  this as "x is assigned the value 2"
+# x = 3  # some languages use different <-
+# print(x)
+
+# this won't work   7 = x
+# print(x)
+
+#
+# num1 = 3
+# num2 = 4
+# num3 = 5.2
+# num4 = 7.1
+# answer1 = num1 - num2
+# answer2 = num2 - num3
+# answer3 = num3 - num4
+# print("The value of answer1 is", answer1)
+# print("The value of answer2 is", answer2)
+# print("The value of answer3 is", answer3)
+
+
+# a = 0.15 + 0.15
+# b = 0.1 + 0.2
+# if a==b:
+# 	print(a, "equals", b)
+# else:
+# 	print(a, "does not equal", b)
+
+
+
+# rays_word = "racecar"
+# backwards_word = "".join(reversed(rays_word))
+# if rays_word == backwards_word:
+# 	print(rays_word, "is a palindrome")
+# else:
+# 	print(rays_word, "is NOT a palindrome")
+
+
+
+# x = input("Enter something: ")
+# y = input("Enter something else: ")
+# print(x + ' + ' + y + ' = ' + (x+y))
+
