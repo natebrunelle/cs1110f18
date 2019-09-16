382b560162905525c530aa43acf0fdb9e2ae66f5
Raymond Pettit <43241221+raypettit57@users.noreply.github.com>
2019-09-16 16:11:56 -0400

    Create Day09.py

---
+++ markdown/files/cs1110-001f19/Day09.py
@@ -0,0 +1,47 @@
+# # Examples used in class on Day09
+#
+# num1 = 1/10  # the / operator always produces a float
+# print(num1)
+#
+# print(num1 * 8)
+# print(num1 + num1 + num1 + num1 + num1 + num1 + num1 + num1)
+#
+# text1 = 'cavaliers'
+# text2 = "cavaliers"
+# print(text1)
+# print(text2)
+
+#text3 = "don't be mean to people"
+
+#text4 = Why "Python"? It's funny!
+#text4 = "Why " + '"Python"? It' + "'s funny!"
+
+# text4 = """Why "Python"? It's funny!
+# Was that a joke?
+# No... OK, maybe."""
+#
+# print(text4)
+
+# text5 = "This should print each letter on a new line:\na\nb\nc"
+# print(text5)
+
+# text6 = "Why \"Python\"?\nIt\'s funny!"
+# print(text6)
+
+# #your_tds = int(input("How many touchdowns do you think we'll score this weekend? "))
+# your_tds = input("How many touchdowns do you think we'll score this weekend? ")
+# #your_tds = int(your_tds)  # this is a type conversion, or casting
+# print(type(your_tds))
+#
+# my_tds = your_tds + your_tds
+# print(type(my_tds))
+# print("You think we'll score", your_tds, "but I think we'll score twice that many,", my_tds)
+
+# 
+# # type casting then checking our new type to confirm
+# x = '7.999999'
+# print(x, "is of type", type(x))
+# y = float(x)
+# print(y, "is of type", type(y))
+# z = int(y)
+# print(z, "is of type", type(z))
