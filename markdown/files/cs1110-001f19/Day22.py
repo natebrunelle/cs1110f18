0f1603737a21fbbdcadce533cb6dcf9355e79690
Raymond Pettit <43241221+raypettit57@users.noreply.github.com>
2019-10-18 15:36:56 -0400

    Create Day22.py

---
+++ markdown/files/cs1110-001f19/Day22.py
@@ -0,0 +1,71 @@
+
+# Idea: loop through some collection and "accumulate" some stuff
+# Start with "nothing"
+# Repeat:
+# 	"Add" something
+# Done: Have it all
+
+
+# integers - accumulate a sum
+
+
+
+# integers - accumulate a product
+
+
+
+# strings - accumulate all text
+
+
+
+
+
+
+
+# add 1 item to a string
+# add 1 item to a list
+# add 1 item to a tuple
+# add 1 item to a range
+
+
+
+# add several items to a string
+# add several items to a list
+# add several items to a tuple
+# add several items to a range
+
+
+
+# # looking at collections ...
+# string1 = "1234"
+# tuple1 = (1,2,3,4)
+# list1 = [1,2,3,4]
+# range1 = range(1,5)
+#
+# my_group = [string1, tuple1, list1, range1]
+#
+# def collection_info(the_collection):
+# 	print(type(the_collection), the_collection)
+# 	for each in the_collection:
+# 		print(type(each), each)
+#
+# print("my_group - ", my_group)
+# for item in my_group:
+# 	collection_info(item)
+
+# There are lots of things that we can do with collections:
+# create
+# access an item - range of indexes, positive and negative?
+# change an item value
+# iterate (loop) through each item
+# check if a given item exists
+# find the length
+# add an item
+# remove an item
+# join 2 collections
+# copy the collection
+
+
+
+
+
